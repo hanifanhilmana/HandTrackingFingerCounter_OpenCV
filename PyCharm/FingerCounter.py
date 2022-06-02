45e3710cfbe934ffc06483b84c65e6090b4dccda
FerdiAgusPurwanda <purwandaagus12@gmail.com>
2022-06-02 16:45:19 +0700

    Inisialisasi Program Camera

--- PyCharm/FingerCounter.py
+++ PyCharm/FingerCounter.py
@@ -111,3 +111,12 @@ while True:
         #cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
         #cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN,
                     #10, (255, 0, 0), 25)
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN,
+                3, (255, 0, 0), 3)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
