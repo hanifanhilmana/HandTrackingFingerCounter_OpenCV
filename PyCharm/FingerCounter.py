a475d7909aab7f1dacea2c5e1d22875d389be4ca
TiaraFadilaAndardini <tiara.andardini@mhs.unsoed.ac.id>
2022-06-02 16:34:25 +0700

    Fungsi Perulangan FingerCounter

--- PyCharm/FingerCounter.py
+++ PyCharm/FingerCounter.py
@@ -15,3 +15,29 @@ pTime = 0
 detector = htm.handDetector(detectionCon=0.75)
 
 tipIds = [4, 8, 12, 16, 20]
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    # print(lmList)
+
+    if len(lmList) != 0:
+        fingers = []
+
+        # Thumb
+        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # 4 Fingers
+        for id in range(1, 5):
+            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        print(fingers)
+        totalFingers = fingers.count(1)
+        #print(totalFingers)
