d8dc6de44eb0ef7c7457480ef65e00c42db773bf
deva1231 <devaputra777@gmail.com>
2022-06-04 22:01:07 +0700

    Comment FingersCounter

--- PyCharm/FingerCounter.py
+++ PyCharm/FingerCounter.py
@@ -3,31 +3,31 @@ import time #import fungsi untuk waktu
 import os #import tampilan pada operasi sistem
 import PyCharmModuleHandTracking as htm #memanggil header PyCharmModuleHandTracking
 
-wCam, hCam = 640, 480 #
+wCam, hCam = 640, 480 #mengatur ukuran kamera
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) #inisialisasi cam openvc yang dipakai
 cap.set(3, wCam)
 cap.set(4, hCam)
 
 
-pTime = 0
+pTime = 0 #inisialisasi nilai awal waktu
 
-detector = htm.handDetector(detectionCon=0.5) #fingsi untuk mendeteksi id koordinat jari
+detector = htm.handDetector(detectionCon=0.5) #fungsi untuk mendeteksi id koordinat jari
 
 tipIds = [4, 8, 12, 16, 20] #koordinat dari x,y pada jari
 
-while True:
-    success, img = cap.read()
-    img = detector.findHands(img)
-    lmList = detector.findPosition(img, draw=False)
-    img = cv2.flip(img,1)
+while True: #fungsi pengkondisian
+    success, img = cap.read() #pembacaan gambar pada kamera yang telah diinisialisasi
+    img = detector.findHands(img) #assign img dengan methode detector
+    lmList = detector.findPosition(img, draw=False) #inisialisasi nilai lmList yang didapat dari methode detector
+    img = cv2.flip(img,1) #menampilkan letak gambar yang divisualkan oleh opencv
     # print(lmList)
 
-    if len(lmList) != 0:
-        fingers = []
+    if len(lmList) != 0: #pengkondisian dengan mengembalikan jumlah item dalam objek lmList tidak sama dengan 0, maka
+        fingers = [] #assign fingers bertipe list
 
         # Thumb
-        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
+        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]: #pengkondisian nilai lmList
             fingers.append(1)
         else:
             fingers.append(0)
